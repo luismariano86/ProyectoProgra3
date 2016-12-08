3462c06b49bbf767b3c756ea449dfb1bffc93ca0
luismariano86 <luismariano86@gmail.com>
2016-12-07 20:44:54 -0600

    Cambios

--- 04Presentacion/frmLibro.Designer.vb
+++ 04Presentacion/frmLibro.Designer.vb
@@ -22,16 +22,58 @@ Partial Class frmLibro
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(97, 64)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(97, 182)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(97, 146)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(97, 107)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 3
         '
         'frmLibro
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(637, 383)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Name = "frmLibro"
         Me.Text = "frmLibro"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
 End Class
--- 04Presentacion/frmUsuario.vb
+++ 04Presentacion/frmUsuario.vb
@@ -1,9 +1,70 @@
-﻿Public Class frmUsuario
+﻿Imports _03Entidades
+Imports _02LogicaNegocios
+
+
+Public Class frmUsuario
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
     End Sub
 
     Private Sub btnInsertar_Click(sender As Object, e As EventArgs) Handles btnInsertar.Click
+        Try
+            Dim miUsu As New UsuarioEN()
+            miUsu.Login = txtLogin.Text
+            miUsu.NombreCompleto = txtNombre.Text
+            If rbtnAdministrador.Checked Then
+                miUsu.IndAdministrador = True
+            End If
+            If rbtnBibliotecario.Checked Then
+                miUsu.IndBibliotecario = True
+            End If
+            If rbtnNo.Checked Then
+                miUsu.IndActivo = False
+            ElseIf rbtnSi.Checked Then
+                miUsu.IndActivo = True
+            End If
 
+            Dim valida As New UsuarioLN
+            valida.InsertarUsuario(miUsu)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Try
+            Dim strLogin As String
+            strLogin = InputBox("Digite el Login del usuario a buscar")
+
+            Dim miUsu As UsuarioEN = Nothing
+
+            Dim valida As New UsuarioLN
+            miUsu = valida.ObtenerUsuarioPorLogin(strLogin)
+
+            If Not IsNothing(miUsu) Then
+                txtLogin.Text = miUsu.Login
+                txtNombre.Text = miUsu.NombreCompleto
+
+                If miUsu.IndAdministrador = True Then
+                    rbtnAdministrador.Checked = True
+                End If
+
+                If miUsu.IndBibliotecario = True Then
+                    rbtnBibliotecario.Checked = True
+                End If
+
+                If miUsu.IndActivo Then
+                    rbtnSi.Checked = True
+                Else
+                    rbtnNo.Checked = True
+                End If
+            Else
+                MessageBox.Show("No existe")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 End Class
